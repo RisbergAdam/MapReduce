484e8c130f88da1a0142eb2ad9ee2978d8bc4bd1
Adam Risberg <risbergadam@gmail.com>
2015-12-13 14:47:57 +0100

    placeholder

--- src/mapreduce/MapReduceExample.java
+++ src/mapreduce/MapReduceExample.java
@@ -7,44 +7,7 @@ import java.util.Scanner;
 public class MapReduceExample {
 
     public static void main(String [] arg) {
-    	MapReduce<String, Integer> master = new MapReduce<>(1, 1);
-        master.applyMap(new TestMap(), "input");
-        master.applyReduce(new TestReduce(), "output");
-        master.killThreads();
-    }
-    
-}
-
-class TestMap implements Map<String, Integer> {
-
-    @Override
-    public void map(String fileName, String fileContent, Emitter<String, Integer> emitter) {
-    	/*String [] words = fileContent.split(" ");
-    
-    	for (String word : words) {
-        	emitter.emit(word.toLowerCase().replace("\n", ""), 1);
-        }*/
     	
-    	System.out.println(fileName);
-    	
-    	Scanner sc = new Scanner(fileContent);
-    	
-    	while (sc.hasNext()) emitter.emit(sc.next().toLowerCase().replace("\n", ""), 1);
-
-    }
-    
-}
-
-class TestReduce implements Reduce<String, Integer> {
-
-    public String reduce(String key, Integer [] values) {
-        int total = 0;
-        
-        for (Integer i : values) {
-            total += i;
-        }
-        
-        return key + ": " + total;
     }
     
 }
