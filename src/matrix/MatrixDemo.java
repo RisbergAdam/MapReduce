63b2a53249dad1234220cbf7d5810b8f5cbcb650
Adam Risberg <risbergadam@gmail.com>
2015-12-15 11:44:49 +0100

    shorter demo, better for Everyone

--- src/matrix/MatrixDemo.java
+++ src/matrix/MatrixDemo.java
@@ -4,26 +4,21 @@ public class MatrixDemo {
 
     public static void main(String [] arg) {
         //initialize the MapReduce framework
-        MatrixReduce matrixReduce = new MatrixReduce(2, 2);
+        MatrixReduce matrixReduce = new MatrixReduce(1, 1);
         
         //create two matrices
-        Double [][] matrix1Values = {{1.0, 2.0, 0.0}, {2.0, 3.0, 3.0}, {-1.0, 1.0, 2.0}};
-        Double [][] matrix2Values = {{0.0, 1.0, -1.0}, {1.0, 0.0, 2.0}, {3.0, -3.0, 3.0}};
+        Double [][] matrixValues = {{1.0, 2.0, 0.0}, {2.0, 3.0, 3.0}, {-1.0, 1.0, 2.0}};
         
-        Matrix matrix1 = new Matrix(matrix1Values);
-        Matrix matrix2 = new Matrix(matrix2Values);
+        Matrix matrix = new Matrix(matrixValues);
         
         //perform some operations using the MapReduce framework
-        Matrix matrix3 = matrixReduce.multiply(matrix1, matrix2);
-        Double determinant = matrixReduce.determinant(matrix3);
-        Matrix matrix3Inv = matrixReduce.inverse(matrix3);
-        Matrix maybeUnit = matrixReduce.multiply(matrix3Inv, matrix3);
+        Matrix matrixInv = matrixReduce.inverse(matrix);
+        Matrix maybeUnit = matrixReduce.multiply(matrixInv, matrix);
         
         //print results of operations
-        System.out.println("Determinant of matrix3: " + determinant + "\n");
-        System.out.println("matrix3:\n" + matrix3.toString());
-        System.out.println("matrix3Inv:\n" + matrix3Inv.toString());
-        System.out.println("matrix3Inv * matrix3:\n" + maybeUnit.toString());
+        System.out.println("matrix:\n" + matrix.toString());
+        System.out.println("matrixInv:\n" + matrixInv.toString());
+        System.out.println("matrixInv * matrix:\n" + maybeUnit.toString());
         
         //this is needed to 
         matrixReduce.dispose();
